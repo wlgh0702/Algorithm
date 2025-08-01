028a6901aa872a534188149bad1ac3b39096793e
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-08-01 21:28:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String lower = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        if(lower.length() <= 1) return true;
+        
+        int j = lower.length() - 1;
+        for(int i = 0; i < s.length() / 2; i++) {
+            if(lower.charAt(i) != lower.charAt(j - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
