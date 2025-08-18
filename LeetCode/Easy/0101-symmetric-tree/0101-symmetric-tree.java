c43ad2d4c9b2a46d6559d227bc9cccb5bd32b74d
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-08-18 19:16:40 +0900

    Time: 0 ms (100%), Space: 41.6 MB (83.89%) - LeetHub

--- LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.java
+++ LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,32 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 import java.util.*;
 
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return helper(root.left,root.right);
+        return isMirror(root.left, root.right);
     }
-    public boolean helper(TreeNode t1, TreeNode t2) {
-        if(t1==null && t2==null)
-            return true;
-        if(t1==null || t2==null)
-            return false;
-        //3rd case t1!=null and t2!=null
-        return t1.val==t2.val && 
-            helper(t1.left,t2.right) && 
-            helper(t1.right,t2.left);
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+
+        if(left == null || right == null) return false;
+
+        boolean boolOne = isMirror(left.left, right.right);
+        boolean boolTwo = isMirror(left.right, right.left);
+
+        return left.val == right.val && boolOne && boolTwo;
     }
 }
