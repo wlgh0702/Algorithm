9ca0ba7d8fa82765c07bf51692e2d9f161af1178
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-08-21 10:21:08 +0900

    Time: 25 ms (65.66%), Space: 40.8 MB (17.71%) - LeetHub

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.java
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.java
@@ -2,10 +2,10 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1;
         int end = n;
-        while(start <= end) {
+        while(start < end) {
             int mid = start + (end - start) / 2;
             if(isBadVersion(mid)) {
-                end = mid - 1;
+                end = mid;
             } else {
                 start = mid + 1;
             }
