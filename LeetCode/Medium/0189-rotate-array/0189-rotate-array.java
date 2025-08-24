09f1f87f707ade3697ab5066f504ac5aa5650384
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-08-24 21:08:32 +0900

    Time: 1 ms (45.89%), Space: 57.5 MB (53.03%) - LeetHub

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
@@ -1,17 +1,14 @@
-import java.util.*;
-
 class Solution {
     public void rotate(int[] nums, int k) {
-        
         k = k % nums.length;
-        int[] rotate = new int[nums.length];
+        int[] temp = new int[nums.length];
 
         for(int i = 0; i < nums.length; i++) {
-            rotate[(i + k) % nums.length] = nums[i];
+            temp[(i + k) % nums.length] = nums[i];
         }
-
+        
         for(int i = 0; i < nums.length; i++) {
-            nums[i] = rotate[i];
+            nums[i] = temp[i];
         }
     }
 }
