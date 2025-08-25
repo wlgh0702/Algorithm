fb1c3606bc5520bc238b6e02e973a34737738424
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-08-25 11:51:46 +0900

    Time: 14 ms (61.78%), Space: 57.9 MB (88.75%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length == 1) return false;
-
         Set<Integer> set = new HashSet<>();
-        for(int i : nums) {
-            if(set.contains(i)) {
+
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
                 return true;
-            } else {
-                set.add(i);
             }
+            set.add(nums[i]);
         }
+
         return false;
     }
 }
