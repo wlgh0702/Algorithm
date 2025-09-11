2dee67a21e4f92adf1673c860ebfc33c4c76c4a4
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-09-11 21:40:56 +0900

    [level 2] Title: 올바른 괄호, Time: 23.54 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        
+        if(s.charAt(0) == ')') return false;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+            if(s.charAt(i) == ')' && stack.size() > 1) {
+                stack.pop();
+                stack.pop();
+            }
+        }
+        if(stack.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
