ed9cc970f640d3b8fb0f70180f5f056e3a4b0fe0
ji_h072 <129346651+wlgh0702@users.noreply.github.com>
2025-09-13 21:51:18 +0900

    [level 1] Title: 폰켓몬, Time: 10.21 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,16 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         Map<Integer, Integer> ponketmon = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             ponketmon.put(nums[i], ponketmon.getOrDefault(nums[i], 0) + 1);
         }
-        if(ponketmon.size() >= nums.length / 2) {
-            answer = nums.length / 2;
-        } else {
-            answer = ponketmon.size();
-        }
-        return answer;
+        return ponketmon.size() >= nums.length / 2? nums.length / 2 : ponketmon.size();
     }
 }
